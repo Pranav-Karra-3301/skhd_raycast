e32c8b23e282dd1ef7201baad6a1e0ddef62fb4e
Pranav Karra <pranavkarra001@gmail.com>
2024-08-01 20:09:34 +0530

    Initial commit

---
+++ raycast-env.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `commands` command */
+  export type Commands = ExtensionPreferences & {}
+  /** Preferences accessible in the `open-skhd-config-file` command */
+  export type OpenSkhdConfigFile = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `commands` command */
+  export type Commands = {}
+  /** Arguments passed to the `open-skhd-config-file` command */
+  export type OpenSkhdConfigFile = {}
+}
+
+
